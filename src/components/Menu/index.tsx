0a5675b945fd7b5c3f4ce9279229e3908ccb2d18
Erick Bernardo <erickvictor@gmail.com>
2021-06-23 12:09:58 -0400

    Add logic open/close to menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Menu as MenuIcon } from '@styled-icons/ionicons-solid/Menu'
 import { Search as SearchIcon } from '@styled-icons/ionicons-solid/Search'
+import { Close as CloseIcon } from '@styled-icons/ionicons-solid/Close'
 import * as S from './styles'
 import Logo from '../Logo'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <Logo />
-    <S.IconWrapper>
-      <SearchIcon aria-label="Search" />
-    </S.IconWrapper>
-  </S.Wrapper>
-)
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
+      </S.IconWrapper>
+      <Logo />
+      <S.IconWrapper>
+        <SearchIcon aria-label="Search" />
+      </S.IconWrapper>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
